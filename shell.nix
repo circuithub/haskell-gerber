b508936a79cd128348d97f5fb3eb1ecff7ef3b8d
Oliver Charles <ollie@ocharles.org.uk>
2018-03-16 10:41:19 +0000

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,29 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, containers, lens, linear, megaparsec
+      , mtl, pretty-show, recursion-schemes, stdenv, text, transformers
+      }:
+      mkDerivation {
+        pname = "gerber";
+        version = "0.1.0.0";
+        src = ./.;
+        libraryHaskellDepends = [
+          base containers lens linear megaparsec mtl pretty-show
+          recursion-schemes text transformers
+        ];
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
